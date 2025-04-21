9f0c60f8fac9a9fb03005988fbbcac6b1d115cfb
Cmmills17 <Cameronmills0713@gmail.com>
2025-04-21 17:37:00 -0400

    Fixed FizzBuzz

--- js/site.js
+++ js/site.js
@@ -1,52 +1,66 @@
 
 function getValues() {
     // - get each <input /> by ID
-    let startInput = document.getElementById('startInput');
-    let endInput = document.getElementById('endInput');
+    // Divi Input
+    let startInputElement = document.getElementById('startInput');
+
+    //Buzz input
+    let endInputElement = document.getElementById('endInput');
 
     // - get the value out of the input
-    let startNumber = parseInt(startInput.value);
-    let endNumber = parseInt(endInput.value);
-    // if the input is not a number, we get NaN
+    let startNumber = parseInt(startInputElement.value);
+    let endNumber = parseInt(endInputElement.value);
+
 
     // - validate that inputs are numbers
-    if (Number.isInteger(startNumber) && Number.isInteger(endNumber)) {
-
-        // generate the numbers between startNumber and endNumber
-        let generatedNumbers = generateNumbers(startNumber, endNumber);
-
-        generatedNumbers;
-        // display the generated numbers
-        displayNumbers(generatedNumbers);
-    } else {
-        // - tell the user to enter valid numbers
+    if (Number.isInteger(startNumber) == false || Number.isInteger(endNumber) == false) {
+        // if not, show the user an error
         Swal.fire(
             {
                 backdrop: false,
                 title: 'oops',
-                text: 'Please enter valid start and end numbers',
+                text: 'Please enter valid Divv and Buzz numbers',
                 icon: 'error',
             }
         );
+
+    } else {
+
+        // generate the numbers between startNumber and endNumber
+        let generatedNumbers = generateNumbers(startNumber, endNumber);
+
+
+        // display the generated numbers
+        displayNumbers(generatedNumbers);
+
     }
 }
 
 // generate some numbers
 function generateNumbers(start, end) {
+
     let numbers = [];
 
-    for (let i = start; i <= end; i++) {
-        if (i === 0) {
-            numbers.push("fizzbuzz"); // Special case for 0
-        } else if (i % 15 === 0) {
-            numbers.push("fizzbuzz");
-        } else if (i % 3 === 0) {
-            numbers.push("fizz");
-        } else if (i % 5 === 0) {
-            numbers.push("buzz");
+    for (let i = 1; i <= 100; i = i + 1) {
+
+
+
+        if (i % start == 0 && i % end == 0) {
+            numbers.push('DivvBuzz');
+
+        } else if (i % start == 0) {
+            numbers.push('Divv');
+
+        } else if (i % end == 0) {
+            numbers.push('Buzz');
+
         } else {
             numbers.push(i);
+
         }
+
+
+
     }
 
     return numbers;
@@ -55,48 +69,25 @@ function generateNumbers(start, end) {
 // display the numbers on the page
 function displayNumbers(numbers) {
 
+    let tableElement = document.getElementById('resultsTable');
+
+
     let tableHtml = '';
 
 
     //go through each number in the array
     for (let index = 0; index < numbers.length; index = index + 1) {
         // -get one number out of the array
-        let currentNumber = numbers[index]
+        let currentNumber = numbers[index];
 
-        let className = '';
-
-
-
-        if (currentNumber % 3 == 0) {
-            // the number is div by 3
-            className = 'fizz';
-
-
-
-        } if (currentNumber % 5 == 0) {
-            //the number is div by 5
-            className = 'buzz';
-
-        } if (currentNumber % 3 == 0 && currentNumber % 5 == 0) {
-            // the number is div by 3 and 5
-            className = 'fizzbuzz';
-
-        }
+        let className = currentNumber;
 
 
         //write the HTML for that number
         tableHtml = tableHtml + `<tr><td class="${className}">${currentNumber}</td></tr>`;
     }
 
-
-
-    // - figure out where to display them, in the table
-    let tableElement = document.getElementById('resultsTable');
-
     //after, put that HTML in the table
     tableElement.innerHTML = tableHtml;
-
-    // - TODO: bold the even numbers
-
 
 }
